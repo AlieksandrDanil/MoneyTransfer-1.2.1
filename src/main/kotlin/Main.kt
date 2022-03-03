991821612aecb60af1dbf96586fc4b836775af9d
AlieksandrDanil <inspcrimea@gmail.com>
2022-03-03 13:19:08 +0300

    Added file MainKt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
+
 fun main() {
-    println("Run Money Transfer")
+    val moneyTransferAmount: Int = 4830_00
+    val commissionFactor: Int = 75
+    val commissionMin: Int = 35_00
+
+    val amount = if (moneyTransferAmount * commissionFactor / 10000 <= commissionMin) (moneyTransferAmount + commissionMin)
+    else (moneyTransferAmount + (moneyTransferAmount * commissionFactor / 10000))
+
+    println("Сумма перевода $moneyTransferAmount с комиссией ${amount - moneyTransferAmount} составит: = $amount копеек")
 }
